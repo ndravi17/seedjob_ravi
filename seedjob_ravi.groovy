99eea4e93ec1f4405558acaa25441f7192711d84
ndravi17 <ndravi17@gmail.com>
2017-03-18 09:56:01 +0530

    Create seedjob_ravi.groovy

---
+++ seedjob_ravi.groovy
@@ -0,0 +1,14 @@
+multibranchPipelineJob('seedjob_ravi') {
+    branchSources {
+        git {
+            remote('https://github.com/jenkinsci/job-dsl-plugin.git')
+            credentialsId('github-ci')
+            includes('JENKINS-*')
+        }
+    }
+    orphanedItemStrategy {
+        discardOldItems {
+            numToKeep(20)
+        }
+    }
+}
