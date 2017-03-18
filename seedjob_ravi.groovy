9461baf056cb0d8f95980bbb6e20d86318ee75f1
ndravi17 <ndravi17@gmail.com>
2017-03-18 10:22:32 +0530

    Update seedjob_ravi.groovy

--- seedjob_ravi.groovy
+++ seedjob_ravi.groovy
@@ -3,8 +3,8 @@ multibranchPipelineJob('seedjob_ravi1') {
         git {
             //*remote('https://github.com/jenkinsci/job-dsl-plugin.git')
             remote('https://github.com/ndravi17/seedjob_ravi.git')
-            credentialsId('github-ci')
-            includes('JENKINS-*')
+            //credentialsId('github-ci')
+            //includes('JENKINS-*')
         }
     }
     orphanedItemStrategy {
